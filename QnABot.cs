04f76c72ec8509fcc75ee4d65fe1073166611133
Valeriia Savenko <vasavenk@microsoft.com>
2018-07-04 10:50:20 +1000

    first commit

---
+++ QnABot.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace AspNetCore_QnA_Bot
+{
+    public class QnABot : IBot
+    {        
+        public async Task OnTurn(ITurnContext context)
+        {
+            if (context.Activity.Type == ActivityTypes.Message && !context.Responded)
+            {
+                await context.SendActivity("No QnA Maker answers were found.");
+            }
+        }
+    }    
+}
